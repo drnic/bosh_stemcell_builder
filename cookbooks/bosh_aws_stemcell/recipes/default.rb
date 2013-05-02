197d583d9ecb614681dc80ea7277a0276d3386f9
Dr Nic Williams <drnicwilliams@gmail.com>
2013-05-01 22:35:31 -0700

    use bosh_base_apt for aws stemcells

--- cookbooks/bosh_aws_stemcell/recipes/default.rb
+++ cookbooks/bosh_aws_stemcell/recipes/default.rb
@@ -1,3 +1,3 @@
-%w[bosh_sysstat bosh_sysctl bosh_ntpdate bosh_sudoers bosh_clean].each do |name|
+%w[bosh_base_apt bosh_sysstat bosh_sysctl bosh_ntpdate bosh_sudoers bosh_clean].each do |name|
   include_recipe name
 end
